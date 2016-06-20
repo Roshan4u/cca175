1a6108dbf995d331928c546fce0065e0185ea078
v.b <vukelan@gmail.com>
2016-06-20 23:11:27 +0300

    wrapped lines

--- commands.sh
+++ commands.sh
@@ -6,4 +6,7 @@
 spark-submit --class "SimpleApp" --master local[4] target/sparkwordcount-0.0.1-SNAPSHOT.jar
 
 # runs spark-submit job with fromClouderaApp.scala
-spark-submit --class SparkWordCount --master local[2] --deploy-mode client --executor-memory 1g --name wordcount --conf "spark.app.id=wordcount" ./target/sparkwordcount2-0.0.1-SNAPSHOT.jar inputfile.txt 2
+spark-submit --class SparkWordCount --master local[2] \ 
+--deploy-mode client --executor-memory 1g --name wordcount \
+--conf "spark.app.id=wordcount" \
+./target/sparkwordcount2-0.0.1-SNAPSHOT.jar inputfile.txt 2
